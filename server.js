ab9f16d5d434d0ee19d7d82107755fd61afb9036
0thman3698 <mohamed.othman3698@gmail.com>
2025-04-23 15:39:05 +0200

    update connecting to DB

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
-mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
